43418e52d2b2375c06540729465ec1f75374c515
AarthiSaminathan <127826312+AarthiSaminathan@users.noreply.github.com>
2023-03-24 15:08:54 +0530

    Add files via upload

---
+++ Cities.cs
@@ -0,0 +1,17 @@
+namespace CsvDemo
+{
+    public class Cities
+    {   
+        public string city{get;set;}
+        public string city_ascii{get;set;}
+        public string lat{get;set;}
+        public string lng{get;set;}
+        public string country{get;set;}
+        public string iso2{get;set;}
+        public string iso3{get;set;}
+        public string admin_name{get;set;}
+        public string capital{get;set;}
+        public string population{get;set;}
+        public long id{get;set;}
+    }
+}
